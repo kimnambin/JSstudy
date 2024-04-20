da99c035296d66439056e64609e0fa69b60601dc
kimnambin <mkkim044@naver.com>
2024-04-20 21:28:52 +0900

    자릿수 더하기

--- lv1/80up.js
+++ lv1/80up.js
@@ -23,3 +23,30 @@ function solution(n) {
     }
     return answer;
 }
+
+//<자릿수 더하기>
+function solution(n){
+    var answer = String(n).split('');
+    let cnt = [];
+    let sum = 0;
+    for(let i =0; i < answer.length; i ++){
+        cnt.push(Number(answer[i]))
+    }
+    for(let k =0; k < cnt.length; k ++){
+        sum += cnt[k]
+    }
+    return sum;
+}
+//문제는 풀었는데 넘 더럽게(?) 푼거 같다 ㅠㅠ
+//문자열 -> 숫자열로 cnt에 푸시 -> cnt에서 합을 sum에 넣기 이런식으로 풀었는데
+function solution(n)
+{
+    var a = (n + '').split('');
+    var b = 0;
+    for(var i = 0; i < a.length; ++i) {
+        b += parseInt(a[i]);
+    }
+    return b;
+}
+//내 풀이에 더 짧은 예시이다 그냥 새로운 배열에 넣지 말고 내 기준 sum에 숫자로 바꾸고 바로 += 했으면 됐는데 바보 같이 어렵게 풀었다 ㅠㅠ
+//요즘 시험기간이라 코테는 못했는데 그사이에 감을 조금 잃은 거 같다 ㅠㅠ
