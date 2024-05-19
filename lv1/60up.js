085174c3ceea688d1bc996356f32d566d7d7ec51
kimnambin <mkkim044@naver.com>
2024-05-19 14:42:34 +0900

    2016년

--- lv1/60up.js
+++ lv1/60up.js
@@ -6,3 +6,12 @@ function solution(nums) {
     else return newnum;
 }
 //이제 정답률 60%대로 가서 긴장했는데 잘풀려서 기분이 좋았다 ㅎㅎ
+
+//2016년
+function solution(a, b) {
+    const days = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
+    var answer = new Date(2016,a-1,b);
+    let result = answer.getDay();
+    return days[result];
+}
+//이 문제는 어렵진 않았는데 date() 객체를 올만에 사용해서 오래 걸렸다ㅠ
