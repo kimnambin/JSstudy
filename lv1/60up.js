7df6a590440867601abe54daad36971abe5178bc
kimnambin <mkkim044@naver.com>
2024-05-19 10:38:57 +0900

    포켓몬

---
+++ lv1/60up.js
@@ -0,0 +1,8 @@
+//포켓몬
+function solution(nums) {
+    var answer = [...new Set(nums)];
+    let newnum = nums.length/2
+    if(newnum >= answer.length) return answer.length;
+    else return newnum;
+}
+//이제 정답률 60%대로 가서 긴장했는데 잘풀려서 기분이 좋았다 ㅎㅎ
