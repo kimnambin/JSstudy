80df3dcac3dc75861978c5487d020591e86d794f
nambin <mkkim044@naver.com>
2024-07-03 12:06:13 +0900

    옹알이(2)

--- lv1/60up.js
+++ lv1/60up.js
@@ -207,3 +207,29 @@ function solution(N, stages) {
     return result.map((x)=>x[0])
 }
 //이게 실패율을 구할 때 분모가 계속 달라지는 경우라 넘 어려웠다 ㅠㅠㅠ
+
+
+//옹알이(2)
+function solution(babbling) {
+    const can = ['aya','ye','woo','ma'];
+    let count = 0;
+    
+    for(let i = 0; i < babbling.length; i++){
+        let babble = babbling[i];
+        
+        for(let j = 0; j < can.length; j++){
+            if(babble.includes(can[j].repeat(2))){
+                break;
+            }
+            
+            babble = babble.split(can[j]).join(" ");
+        }
+        
+        if(babble.split(" ").join("").length === 0){
+            count += 1;
+        }
+    }
+    
+    return count;
+}
+//안쓰던 repeat(새로운 문자열 반환) 함수도 사용하고 올만에  split와 includes를 사용한 문제였다 
