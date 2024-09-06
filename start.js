48b74717e414ef47381e3aedf34b9578ab443419
nambin <mkkim044@naver.com>
2024-09-06 12:10:10 +0900

    09월 06일 햄버거 복습

--- start.js
+++ start.js
@@ -1,34 +1,17 @@
 // const input = require('fs').readFileSync('/dev/stdin').toString().split('');.split(/\s+/);
 
-//2563번 색종이
+//백준  2292 벌집
 
-const input = require('fs')
-  .readFileSync('C:\\Users\\82109\\Desktop\\Study\\JSstudy\\input.txt')
-  .toString()
-  .trim()
-  .split('\n');
+const input = require('fs').readFileSync('/dev/stdin').toString().trim();
 
-// 2차원 배열 만들기
-let map = Array.from(Array(100), () => Array(100).fill(0));
+let num = Number(input);
 
-//가로 , 세로 채워 넣기!!
-for (let i = 1; i <= input[0]; i++) {
-  let [가로, 세로] = input[i].split(' ').map(Number);
+let 번호 = 1;
+let range = 1;
 
-  for (let a = 가로; a < 가로 + 10; a++) {
-    for (let b = 세로; b < 세로 + 10; b++) {
-      map[a][b] = 1;
-    }
-  }
+while (num > range) {
+  range += 번호 * 6;
+  번호++;
 }
 
-let cnt = 0;
-
-//이제 차지하는 공간 구하기
-for (let i = 0; i < 100; i++) {
-  for (let j = 0; j < 100; j++) {
-    if (map[i][j] == 1) cnt++;
-  }
-}
-
-console.log(cnt);
+console.log(번호);
