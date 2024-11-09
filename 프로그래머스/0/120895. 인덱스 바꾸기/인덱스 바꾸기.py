0e9c4a875dd9c03e12081f457c6332aa5f7924a9
pronambin <127464935+kimnambin@users.noreply.github.com>
2024-11-09 13:27:25 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -0,0 +1,12 @@
+def solution(my_string, num1, num2):
+    answer = ''
+
+    for i in range(0,len(my_string)) :
+        if i != num1 and i != num2 :
+            answer += my_string[i]
+        elif i == num1 :
+            answer += my_string[num2]
+        elif i == num2 :
+            answer += my_string[num1]    
+        
+    return answer
