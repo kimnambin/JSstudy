d05bbc7f35ff30d10fca58100baf9be43bc3245c
pronambin <127464935+kimnambin@users.noreply.github.com>
2024-11-27 16:05:21 +0900

    [level 2] Title: 피로도, Time: 6.93 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,18 +1,17 @@
 function solution(k, dungeons) {
-    let answer = 0;
-    let visited = new Array(dungeons.length).fill(false) //방문 여부 확인
-    
-    function DFS(hp,D){ 
-        //hp는 피로도 , D는 깊이(정답)
-        for(let i =0; i < dungeons.length; i++){
-            if(dungeons[i][0] <= hp && !visited[i]){
-                visited[i] = true
-                DFS(hp - dungeons[i][1] , D+1)
-                visited[i] = false
-            }
-        }
-      answer = Math.max(answer,D);
+  let answer = 0;
+  let visited = new Array(dungeons.length).fill(false);
+
+  function DFS(hp, D) {
+    for (let i = 0; i < dungeons.length; i++) {
+      if (!visited[i] && hp >= dungeons[i][0]) {
+        visited[i] = true;
+        DFS(hp - dungeons[i][1], D + 1);
+        visited[i] = false;
+      }
     }
-    DFS(k,0)
-    return answer
+    answer = Math.max(answer, D);
+  }
+  DFS(k, 0);
+  return answer;
 }
